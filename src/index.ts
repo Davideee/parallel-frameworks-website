eaf7ce4a0d7ff1bc0ef44f57bff362ad3d0fc9ed
Davide Di Mauro <davide.dimauro@schleuniger.com>
2022-10-26 10:03:18 +0200

    add rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+/* eslint-disable no-console */
+import { interval, switchMap, tap } from 'rxjs';
 import { mountVanillaApp } from './apps/vanilla/mount';
 
 mountVanillaApp();
+
+const a = interval(1000);
+const b = interval(198);
+
+a.pipe(
+  switchMap((val) => {
+    console.log('Int: ', val);
+    return b;
+  }),
+  tap((val) => console.log('Interval2: ', val))
+).subscribe();
